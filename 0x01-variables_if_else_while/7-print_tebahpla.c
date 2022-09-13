1239d417d76c7acf4777dc18e40a4d30b312d5f7
trevorndegwa <tntrende@gmail.com>
2022-09-12 19:49:06 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char za;
+
+	for (za = 'z'; za >= 'a'; za--)
+	putchar(za);
+	putchar('\n');
+
+	return (0);
+}
