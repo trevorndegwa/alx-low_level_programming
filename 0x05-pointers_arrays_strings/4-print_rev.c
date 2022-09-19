34dcd463527e83deaaf16779f41b36777abecf47
trevorndegwa <tntrende@gmail.com>
2022-09-19 16:55:17 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: value to be evaluated
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
