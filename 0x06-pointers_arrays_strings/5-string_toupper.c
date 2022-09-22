2774a13004ff211dd9a9d6e3bbaed0f743195fdb
trevorndegwa <tntrende@gmail.com>
2022-09-22 15:51:47 -0700

    lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int j;
+
+	j = 0;
+	while (n[j] != '\0')
+	{
+		if (n[j] >= 'a' && n[j] <= 'z')
+			n[j] = n[j] - 32;
+		j++;
+	}
+	return (n);
+}
