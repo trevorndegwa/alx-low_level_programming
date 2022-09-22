0e203e5e659f7446c5b682219de1ae73d3f1aaae
trevorndegwa <tntrende@gmail.com>
2022-09-22 13:49:51 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,22 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int i;
 
-	while (*src)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		srclen++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-	return (temp);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
