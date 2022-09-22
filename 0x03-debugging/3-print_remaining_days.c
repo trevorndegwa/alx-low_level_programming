4fa84f2c7b8fee90cf8e83af7db572c88c110233
trevorndegwa <tntrende@gmail.com>
2022-09-22 03:05:05 -0700

    prints remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left in the year accounting for leap yr
+ * print_remaining_days - takes a date and prints remaining days
  * @month: month in number format
  * @day: day of month
  * @year: year
