dab588e43f87139dbc6ff7b7a09d2a93c64ba7cf
trevor ndegwa <tntrende@gmail.com>
2023-09-18 19:42:04 -0700

    puts2 func

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*puts2 - prints part of a string
+*@str: string to be printed
+*
+* Return: void
+**/
+
+/*
+*1 Function called puts2 that takes a string as an argument
+2 Integer variable called i and initialize it to 0
+3 Create a while loop that will iterate through the string
+4 Inside the while loop, create an if statement that checks i
+5 If the value of i is divisible by 2, print the character at the index i
+6 Increment i by 1
+7 After the while loop, print a new line
+*/
+/*
+*Time Complexity: O(n)
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
