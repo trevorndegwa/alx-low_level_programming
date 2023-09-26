5f64039babda5931ed58f8099352ba6ac9b47e2a
trevor ndegwa <tntrende@gmail.com>
2023-09-26 00:07:13 -0700

    correction for diagonal summation

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - this function prints the sum of 2 diagonals for
- * a square matrix of integers
- * @a: 2-D array
- * @size: size x size - the square matrix
- *
- * Return: nothing
- */
+* print_diagsums - this function prints the sum of 2 diagonals for
+* a square matrix of integers
+* @a: 2-D array
+* @size: size x size - the square matrix
+*
+* Return: nothing
+*/
 
 void print_diagsums(int *a, int size)
 {
-	int priC, sum1, sum2, diagC1, diagC2, row;
+	int j, sum1 = 0, sum2 = 0;
 
-	priC = 0, sum1 = 0, sum2 = 0, diagC1 = 0, diagC2 = 0, row = 0;
-	while (priC < size * size)
+	for (j = 0; j < size; j++)
 	{
-		if (priC == diagC1 + (row * size))
-			sum1 += a[primC];
-		if (priC == (size - diagC2 - 1) + (row * size))
-			sum2 += a[priC];
-		if (((priC + 1) % size) == 0)
-			row++, diagC1++, diagC2++;
-		priC++;
+		sum1 += a[j];
+		a += size;
 	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+
+	a -= size;
+
+	for (j = 0; j < size; j++)
+	{
+		sum2 += a[j];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
