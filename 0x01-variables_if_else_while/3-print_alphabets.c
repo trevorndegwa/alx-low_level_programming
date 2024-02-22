789d220f7f2215aab895052ffb24891aff51e1fc
trevor ndegwa <tntrende@gmail.com>
2024-02-22 01:52:25 -0800

    print lower then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c, d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
