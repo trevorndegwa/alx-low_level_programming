245e0921fb74db64c7cfdad0a43cf212e90e441d
trevor ndegwa <tntrende@gmail.com>
2024-04-01 12:57:32 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+
+void print_alphabet_x10(void);
+{
+	int c;
+	char d;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+	}
+	_putchar('\n');
+}
