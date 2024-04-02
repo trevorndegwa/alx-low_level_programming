a518da126c7a15a83bea561bd1bc3e174bc4b849
trevor ndegwa <tntrende@gmail.com>
2024-04-02 03:50:52 -0700

    print times table for 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,22 @@
 
 void times_table(void)
 {
-	int j, k;
+	int j, k, l;
 
 	for (j = 0; j <= 9; j++)
 	{
 		for (k = 0; k <= 9; k++)
 		{
+			l = k * j;
 			if (k != 9)
 			{
-				_putchar((k * j) + '0');
+				_putchar((l) + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar((k * j) + '0');
+				_putchar((l) + '0');
 				_putchar('$');
 				_putchar('\n');
 			}
