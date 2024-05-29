e77bd0d88a8fedd9fbde28833d7e6a8b0659a3d4
trevor ndegwa <tntrende@gmail.com>
2024-05-29 13:02:27 -0700

    most nos

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != '2' && c != '4')
+		while (c != '2' && c != '4')
 			_putchar(c);
-		else
-			break;
 	}
 	_putchar('\n');
 }
