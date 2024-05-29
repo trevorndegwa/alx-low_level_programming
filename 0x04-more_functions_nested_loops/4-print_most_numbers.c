2c797c7a1c031b42d20c32fe152fc9c9f1fbde81
trevor ndegwa <tntrende@gmail.com>
2024-05-29 13:11:22 -0700

    print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,10 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	while (c != '2' && c != '4')
 	{
-		while (c != '2' || c != '4')
-			_putchar(c);
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
