18e32add034e7832dec0e7ea0d01cbcf805902fe
trevor ndegwa <tntrende@gmail.com>
2024-05-29 12:35:24 -0700

    print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,11 +6,11 @@
  *Return: returns result
  */
 
-/*
-*1. It’s defining a function called mul that takes two integers as arguments and returns an integer.
-2. It’s returning the product of the two arguments.
-*/
 int mul(int a, int b)
 {
-return (a * b);
+	long int result;
+
+	result = a * b;
+
+	return (result);
 }
