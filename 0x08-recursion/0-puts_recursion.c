81c90f36a8d81af1814692efa889c2e990ca6a38
trevor ndegwa <tntrende@gmail.com>
2024-07-21 03:09:55 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints string then new line
+ * @s: character to be printed
+ * Return: returns void
+ */
+
+void _puts_recursion(char *s)
+{
+	if *s == '\0'
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*(s + 1));
+	}
+}
