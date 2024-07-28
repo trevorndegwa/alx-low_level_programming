22f026d7e5c8dac63bddd228f30a46780d1f2ac8
trevor ndegwa <tntrende@gmail.com>
2024-07-28 04:26:18 -0700

    correction to create array assignt

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
+	if (size == 0)
+		return (NULL);
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
