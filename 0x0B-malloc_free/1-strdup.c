f75932d19c14713954a3c9a575b7499559ea9359
trevor ndegwa <tntrende@gmail.com>
2024-07-28 07:35:40 -0700

    concatenate strings with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strdup - function returns pointer to a string copy
+ * @str: string to copy in allocated space
+ * Return: pointer to space on success, NULL if failure
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int len, i;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	array = malloc(len * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		array[i] = str[i];
+	return (array);
+}
