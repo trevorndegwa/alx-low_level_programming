b860e6ca77e20cca93bad96a2116918bfd0ed4cd
trevor ndegwa <tntrende@gmail.com>
2024-07-29 18:17:25 +0300

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	block = malloc(nmemb * size);
 	if (block == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		block[i] = 0;
 	return (block);
 }
