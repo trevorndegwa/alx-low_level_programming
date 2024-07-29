dbee6212195ad4083a43c0dbc110118b241f7779
trevor ndegwa <tntrende@gmail.com>
2024-07-29 18:01:57 +0300

    correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *block;
 	unsigned int i;
 
-	if (nmemb == 0 | size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	block = malloc(nmemb * size);
 	if (block == NULL)
