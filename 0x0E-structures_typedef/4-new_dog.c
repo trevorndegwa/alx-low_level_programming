5a43167a740cf6d5d35370aed3482d62b5b6f1b8
trevor ndegwa <tntrende@gmail.com>
2024-08-06 13:32:52 +0300

    done corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -62,6 +62,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggy->owner == NULL)
 	{
+		free(doggy->name);
 		free(doggy);
 		return (NULL);
 	}
