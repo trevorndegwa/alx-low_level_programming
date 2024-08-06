be16a06d9205274646ed961d928b3770e1c239fe
trevor ndegwa <tntrende@gmail.com>
2024-08-06 14:01:19 +0300

    frees all dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,16 @@
+#include "dog.h"
 
+/**
+ * free_dog - function frees dogs
+ * @d: pointer to dog_t d
+ * return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
