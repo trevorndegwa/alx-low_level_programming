9b51f50e5bb7b5acd43c4b7c50f3a26bf6b301fd
trevor ndegwa <tntrende@gmail.com>
2024-08-08 16:26:59 +0300

    corrections

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char *);
 		if (str == NULL)
