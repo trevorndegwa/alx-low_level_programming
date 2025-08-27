7f2f83d0e7815529e309000bf52b357bae0d3d9c
Trevor Ndegwa <tntrende@gmail.com>
2025-08-27 14:14:15 +0300

    Print lower alphabet characters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
