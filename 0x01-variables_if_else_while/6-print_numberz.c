e1a20d8547217433b504ea6ef975af1c0fff87ae
Trevor Ndegwa <tntrende@gmail.com>
2025-08-27 14:10:53 +0300

    Print digits 0-9 without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
