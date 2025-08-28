3c856f8984ede51338bd0e7504685e2fe0d02c4d
Trevor Ndegwa <tntrende@gmail.com>
2025-08-28 19:45:40 +0300

    Check sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a no.
+ * @n: parameter to be evaluated
+ *
+ * Return: 1 (+ve), 0 (0), -1 (-ve)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+}
