9ca2f78407e68a7e93babed468c3850082776ad4
Trevor Ndegwa <tntrende@gmail.com>
2025-08-28 18:26:15 +0300

    Print alphabet 10x in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet x10 in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c = 'a';
+
+	for (i = 0; i <= 9; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
