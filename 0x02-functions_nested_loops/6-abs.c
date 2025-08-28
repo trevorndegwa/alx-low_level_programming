34cbc506e2156d153a9f9e7a9055ccd32a8e7611
Trevor Ndegwa <tntrende@gmail.com>
2025-08-28 19:58:59 +0300

    Get absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an int
+ * @int: parameter to be checked
+ *
+ * Return: absolute value of int
+ */
+int _abs(int)
+{
+	int i;
+
+	i = int;
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		i = i * -1;
+		return (i);
+	}
+}
