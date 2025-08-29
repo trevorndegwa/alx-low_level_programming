cf3fbbb609c76b67dbffad7912c0c1fb10e88801
Trevor Ndegwa <tntrende@gmail.com>
2025-08-29 10:45:11 +0300

    Edited the big fibonacci problem

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,16 +6,40 @@
  */
 int main(void)
 {
-	long int a = 1, b = 2, c;
+	unsigned long a = 1, b = 2;
+	unsigned long a_high, b_high, a_low, b_low;
+	unsigned long carry, high, low
 	int i;
 
-	printf("%ld, %ld", a, b);
-	for (i = 3; i <= 98; i++)
+	/* print first 2 numbers*/
+	printf("%lu, %lu", a, b);
+
+	/* generate next up to 92nd safely in unsigned long*/
+	for (i = 3; i <= 92; i++)
 	{
-		c = a + b;
-		printf(", %ld", c);
-		a = b;
-		b = c;
+		b = b + a;
+		a = b - a;
+		printf(", %lu", b);
+	}
+	/* split the last a and bs into high and low parts*/
+	a_high = a / 1000000000;
+	a_low = a % 1000000000;
+	b_high = b / 1000000000;
+	b_low = b % 1000000000;
+
+	/* cont from 93rd to 98th*/
+	for (i = 93, i <= 98, i++)
+	{
+		carry = (a_low + b_low) / 1000000000;
+		low = (a_low + b_low) % 1000000000;
+		high = a_high + b_high + carry;
+
+		printf(", %lu%09lu", high, low);
+
+		a_high = b_high;
+		a_low = b_low;
+		b_high = high;
+		a_low = low;
 	}
 	printf("\n");
 	return (0);
