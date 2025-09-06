31bdf96c985df53a0ab45c1b4dcafee3f25e3b11
Trevor Ndegwa <tntrende@gmail.com>
2025-09-06 17:45:49 +0300

    limits for atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -11,13 +12,19 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int found_digit = 0;
+	int digit;
 
 	while (*s != '\0')
 	{
 		if (*s >= '0' && *s <= '9')
 		{
+			digit = *s - '0';
 			found_digit = 1;
-			result = result * 10 + (*s - '0');
+
+			if (sign == 1 && (result > (INT_MAX - digit) / 10))
+				return (INT_MAX);
+			if (sign == -1 && (result < -(INT_MIN + digit) / 10))
+				return (INT_MIN);
 		}
 		else if (found_digit)
 		{
