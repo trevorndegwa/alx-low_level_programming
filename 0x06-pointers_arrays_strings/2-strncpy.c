36e0ef88a020350c467da8dd6ecc82526c3b1b6c
Trevor Ndegwa <tntrende@gmail.com>
2025-09-06 19:55:40 +0300

    copying n bytes to source string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string, including the terminating null byte, using at most n bytes
+ * @dest: destination buffer storing copied string
+ * @src: source string
+ * @n: max integer of src buffer to print
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0'
+	}
+	return (dest);
+}
