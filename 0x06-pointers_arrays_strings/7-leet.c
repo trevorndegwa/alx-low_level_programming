261333d2f7a1967405ee46d2f0f5230fe7e9996d
Trevor Ndegwa <tntrende@gmail.com>
2025-09-07 10:30:35 +0300

    1337 encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char codes[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] == codes[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
