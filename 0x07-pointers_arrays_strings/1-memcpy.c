a0ae8f710941e20e84ead156e1021165e974d0b7
Trevor Ndegwa <tntrende@gmail.com>
2025-09-08 09:29:34 +0300

    Copy memory area to other buffer

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,17 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	char *tmp;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest = src[i];
+		tmp[i] = src[i];
+	}
+	while (*tmp)
+	{
+		*dest = *tmp;
 		dest++;
+		tmp++;
 	}
 	return (dest);
 }
