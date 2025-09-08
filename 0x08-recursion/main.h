1188f672999e1377db5809d9ab8c5ae596ebbaf0
Trevor Ndegwa <tntrende@gmail.com>
2025-09-08 22:33:51 +0300

    Return length of string recursively

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _strlen(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _strlen(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */
