76235427346dab3fca26d0087bf405a256ef53bc
Trevor Ndegwa <tntrende@gmail.com>
2025-09-08 09:24:23 +0300

    Copy memory area to other buffer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies memory area
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: bytes to copy from src buffer
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest = src[i];
+		dest++;
+	}
+	return (dest);
+}
