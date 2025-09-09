0d32892d540c97457909b8e20e428f5b9ab5cd9c
Trevor Ndegwa <tntrende@gmail.com>
2025-09-09 08:32:40 +0300

    Wildcard matching problem

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * wildcmp - copmares 2 string, considering * as a wildcard
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+	{
+		/* Case 1: '*' matches empty string → skip '*' */
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		/* Case 2: '*' matches current char in s1 → move in s1 */
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		return (0);
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int is_prime_number(int n);
 int is_prime_helper(int n, int i);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int start, int end);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
