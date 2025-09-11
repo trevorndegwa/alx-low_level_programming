75b5f1a426bddf95f5c70299c2ba8cb79014da81
Trevor Ndegwa <tntrende@gmail.com>
2025-09-11 14:34:43 +0300

    Create array of chars, initialising it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		ptr[i] = 'H';
+		ptr[i] = c;
 	}
 	return (ptr);
 }
