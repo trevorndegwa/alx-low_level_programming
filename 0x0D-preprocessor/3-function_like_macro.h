b8483ccd8e9762b237e7608f04acdf08aaf12cd1
Trevor Ndegwa <tntrende@gmail.com>
2025-09-19 13:30:04 +0300

    preprocessor tasks

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 
-#endif /* FUNCTION_LIKE_MACRO_H */
+#endif /* FUNCTION_LIKE_MACRO */
