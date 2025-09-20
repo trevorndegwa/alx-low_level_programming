d68a2eb83ef7cf9025824196a97fa4b19f36bc82
Trevor Ndegwa <tntrende@gmail.com>
2025-09-20 11:47:41 +0300

    Fixed NULL handling

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function initialises a variable of type struct dog
@@ -9,7 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return;
 	d->name = name;
