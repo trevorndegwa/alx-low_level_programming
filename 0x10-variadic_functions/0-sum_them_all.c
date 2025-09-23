20949568879c6a31bb15ce2ffa1bedbd997c260b
Trevor Ndegwa <tntrende@gmail.com>
2025-09-23 08:40:19 +0300

    Fixed betty coding style issue

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,8 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, result = 0;
 	va_list ap;
-	va_start(ap, n);
 
+	va_start(ap, n);
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
@@ -20,5 +20,5 @@ int sum_them_all(const unsigned int n, ...)
 		result += va_arg(ap, unsigned int);
 	}
 	va_end(ap);
-	return result;
+	return (result);
 }
