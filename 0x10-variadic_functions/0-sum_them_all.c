bfba732931100314a884a24f44df89e9bdb31f5e
Trevor Ndegwa <tntrende@gmail.com>
2025-09-23 08:37:41 +0300

    Get sum of variadic function parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,24 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - returns sum when giving unkonw args
- * @n: count of undefined arguments
- * Return: sum on success, 0 if no optional args
+ * sum_them_all - function returns sum of its params
+ * @n: count parameter
+ *
+ * Return: Parameter sum or 0
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, result = 0;
 	va_list ap;
-	unsigned int i, sum = 0;
+	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
-
-	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
+	{
+		result += va_arg(ap, unsigned int);
+	}
 	va_end(ap);
-	return (sum);
+	return result;
 }
